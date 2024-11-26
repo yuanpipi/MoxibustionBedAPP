d9c566c168e21b9ac8d978aad4a80a1fa5692684
26821 <26821@YYP-PC>
2024-11-26 19:02:47 +0800

    添加项目文件。

---
+++ MoxibustionBedAPP/Models/RelayCommand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MoxibustionBedAPP.Models
+{
+    public class RelayCommand : ICommand
+    {
+        private Action _execute;
+
+        private Func<bool> _canExecute;
+
+        public RelayCommand(Action execute, Func<bool> canExecute=null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        //public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            //throw new NotImplementedException();
+            return _canExecute == null || _canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            //throw new NotImplementedException();
+            _execute();
+        }
+    }
+}
---
+++ MoxibustionBedAPP/ViewModes/MainWindowViewModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using MoxibustionBedAPP.Models;
+using MoxibustionBedAPP.Views;
+
+namespace MoxibustionBedAPP.ViewModes
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        //public enum UserControlType
+        //{
+        //    FunctionControl,
+        //    DataMonitoring,
+        //    PlayMusic
+        //}
+
+        private UserControl _currentUserControl;
+        public UserControl CurrentUserControl
+        {
+            get
+            {
+                return _currentUserControl;
+            }
+            set
+            {
+                _currentUserControl = value;
+                OnPropertyChanged(nameof(CurrentUserControl));
+            }
+        }
+
+
+        public ICommand ShowFunctionControlCommand
+        {
+            get
+            {
+                return new RelayCommand(() =>
+                {
+                    CurrentUserControl = new FunctionControlView();
+                });
+            }
+        }
+
+        public ICommand ShowDataMonitoringCommand
+        {
+            get
+            {
+                return new RelayCommand(() =>
+                {
+                    CurrentUserControl = new DataMonitoringView();
+                });
+            }
+        }
+
+        public ICommand ShowPlayMusicCommand
+        {
+            get
+            {
+                return new RelayCommand(() =>
+                {
+                    CurrentUserControl = new PlayMusicView();
+                });
+            }
+        }
+
+        public ICommand ShowParameterSettingCommand
+        {
+            get
+            {
+                return new RelayCommand(() =>
+                {
+                    CurrentUserControl = new ParameterSettingView();
+                });
+            }
+        }
+
+    }
+}
